1d106ad6722fe0047a1be99daacf8be1232d45e8
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2023-06-01 15:02:32 +0800

    update

--- Tank/src/main/java/org/jy/tank/Main.java
+++ Tank/src/main/java/org/jy/tank/Main.java
@@ -3,6 +3,8 @@ package org.jy.tank;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.Time;
+import java.util.Timer;
 
 public class Main {
     public static void main(String[] args) {
--- Tank/src/main/java/org/jy/tank/TankFrame.java
+++ Tank/src/main/java/org/jy/tank/TankFrame.java
@@ -1,16 +1,21 @@
 package org.jy.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+    private static final int STEP = 10;
+    int paintX = 10;
+    int paintY = 10;
     public TankFrame() {
         this.setSize(800, 600);
         this.setTitle("Tank War");
         this.setResizable(false);
         this.setVisible(true);
-
+        this.addKeyListener(new FrameKeyListener());
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -21,7 +26,34 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(200, 200, 50, 50);
-//        super.paint(g);
+        g.fillRect(paintX, paintY, 50, 50);
+    }
+
+    class FrameKeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_W:
+                    paintY -= STEP;
+                    break;
+                case KeyEvent.VK_S:
+                    paintY += STEP;
+                    break;
+                case KeyEvent.VK_A:
+                    paintX -= STEP;
+                    break;
+                case KeyEvent.VK_D:
+                    paintX += STEP;
+                    break;
+                default:
+                    break;
+            }
+            repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            super.keyReleased(e);
+        }
     }
 }
