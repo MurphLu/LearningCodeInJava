3db17f63ab24f38c99b331c97fc402a42514114e
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2023-08-24 11:13:27 +0800

    add more

--- DesignPatternsDemos/src/main/java/org/dp/core/App.java
+++ DesignPatternsDemos/src/main/java/org/dp/core/App.java
@@ -21,12 +21,7 @@ public class App {
     public static void strategyTest() {
         StrategySorter<Dog> sorter = new StrategySorter<>();
         Dog[] dogs = new Dog[]{new Dog(2), new Dog(1), new Dog(3)};
-        sorter.sort(dogs, new Comparator<Dog>() {
-            @Override
-            public int compare(Dog o1, Dog o2) {
-                return Integer.compare(o1.getWeight(), o2.getWeight());
-            }
-        });
+        sorter.sort(dogs, (o1, o2) -> Integer.compare(o1.getWeight(), o2.getWeight()));
         System.out.println(Arrays.toString(dogs));
     }
 }
