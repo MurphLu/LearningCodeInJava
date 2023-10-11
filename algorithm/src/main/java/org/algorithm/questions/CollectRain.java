fab66ea0fa1bae6511452c7483f86fe02dd98da0
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2023-10-11 16:04:28 +0800

    add more

---
+++ algorithm/src/main/java/org/algorithm/questions/CollectRain.java
@@ -0,0 +1,8 @@
+package org.algorithm.questions;
+
+public class CollectRain {
+    public static void main(String[] args) {
+
+    }
+
+}
--- algorithm/src/main/java/org/algorithm/slidingWindow/StringQues.java
+++ algorithm/src/main/java/org/algorithm/slidingWindow/StringQues.java
@@ -3,9 +3,10 @@ package org.algorithm.slidingWindow;
 import java.util.HashSet;
 import java.util.Set;
 
-public class longestSubStr {
+public class StringQues {
     public static void main(String[] args) {
-        System.out.println(new longestSubStr().lengthOfLongestSubstring("aabaab!bb"));
+        System.out.println(new StringQues().lengthOfLongestSubstring("aabaab!bb"));
+        System.out.println(StringQues.lengthOfLastWord("   fly me   to   the moon  "));
     }
 
     public int lengthOfLongestSubstring(String s) {
@@ -27,5 +28,17 @@ public class longestSubStr {
             set.add(chars[i]);
         }
         return Math.max(set.size(), length);
+    }
+
+    public static int lengthOfLastWord(String s) {
+//        char[] chars = s.toCharArray();
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c == ' ' && length == 0){ continue; }
+            if (length > 0 && c == ' '){ break; }
+            length ++;
+        }
+        return length;
     }
 }
