16e2964e9e8496a3374b674059b5aeeae30e33aa
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2023-11-06 10:47:19 +0800

    add daily

--- algorithm/src/main/java/org/algorithm/leetcode/daily/MaximumInvitations.java
+++ algorithm/src/main/java/org/algorithm/leetcode/daily/MaximumInvitations.java
@@ -7,14 +7,72 @@ import java.util.*;
 // 如果换的长度等于2，则取到环上两个节点最长距离之和
 public class MaximumInvitations {
     public static void main(String[] args) {
-        System.out.println(maximumInvitations(new int[]{7,0,7,13,11,6,8,5,9,8,9,14,15,7,11,6}));
+        System.out.println(maximumInvitations(new int[]{6,4,4,5,0,3,3}));
         // 1,0,0,2,1,4,7,8,9,6,7,  10, 8
         // 0 1 2 3 4 5 6 7 8 9 10 11   12
     }
 
+
+    static class TwoPointCircleList {
+        static class Node {
+            int val;
+            List<Node> next;
+            int distance;
+
+            public Node(int val, int distance) {
+                this.val = val;
+                this.distance = distance;
+                next = new ArrayList<>();
+            }
+        }
+        Node pointOne;
+        Node pointTwo;
+
+        Map<Integer, Node> edgeOneNodeMap = new HashMap<>();
+        Map<Integer, Node> edgeTwoNodeMap = new HashMap<>();
+        int maxDisToOne;
+        int maxDisToTwo;
+
+        public TwoPointCircleList(int pointOne, int pointTwo) {
+            this.pointOne = new Node(pointOne, 1);
+            this.pointTwo = new Node(pointTwo, 1);
+            edgeOneNodeMap.put(pointOne, this.pointOne);
+            edgeTwoNodeMap.put(pointTwo, this.pointTwo);
+            maxDisToOne = 1;
+            maxDisToTwo = 1;
+        }
+
+        boolean checkAndAdd(List<Integer> map, int pointNeedToCheck) {
+            if (edgeOneNodeMap.containsKey(pointNeedToCheck)) {
+                maxDisToOne = Math.max(maxDisToOne, add(edgeOneNodeMap, map, pointNeedToCheck));
+                return true;
+            } else if (edgeTwoNodeMap.containsKey(pointNeedToCheck)) {
+                maxDisToTwo = Math.max(maxDisToTwo, add(edgeTwoNodeMap, map, pointNeedToCheck));
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private int add(Map<Integer, Node> edge, List<Integer> points, int pointNeedToCheck) {
+            Node node = edge.get(pointNeedToCheck);
+            for (int i = points.size() - 1; i >= 0; i--) {
+                Node newNode = new Node(points.get(i), node.distance + 1);
+                node.next.add(newNode);
+                edge.put(points.get(i), newNode);
+                node = newNode;
+            }
+            return node.distance;
+        }
+
+        public int maxDistance() {
+            return this.maxDisToTwo + this.maxDisToOne;
+        }
+    }
+
     public static int maximumInvitations(int[] favorite) {
         Set<Integer> checked = new HashSet<>();
-        Map<Integer, Integer> twoPointMap = new HashMap<>();
+        List<TwoPointCircleList> twoList = getList(favorite, checked);
         int max = 0;
         for (int i = 0; i < favorite.length; i++) {
             int temp = i;
@@ -22,23 +80,45 @@ public class MaximumInvitations {
                 continue;
             }
             Map<Integer, Integer> map = new HashMap<>();
+            List<Integer> list = new ArrayList<>();
             int index = 0;
-            while (!map.containsKey(temp)) {
+            while (!map.containsKey(temp) && !checked.contains(temp)) {
                 map.put(temp, index++);
+                list.add(temp);
                 temp = favorite[temp];
+                boolean addToTwo =false;
+                for (TwoPointCircleList twoPoint: twoList) {
+                    if (twoPoint.checkAndAdd(list, temp)) {
+                        addToTwo = true;
+                    }
+                }
+                if (addToTwo) {break;}
             }
-            if (index - map.get(temp) == 2) {
-                twoPointMap.put(temp, Math.max(twoPointMap.getOrDefault(temp, 0), index));
-            } else {
-                checked.addAll(map.keySet());
+            if (map.containsKey(temp)) {
+                max = Math.max(index - map.get(temp), max);
             }
-            max = Math.max(index - map.get(temp), max);
+
+            checked.addAll(map.keySet());
+
         }
         int twoMax = 0;
-        for (Map.Entry<Integer,Integer> entry: twoPointMap.entrySet()) {
-            twoMax += (entry.getValue() + twoPointMap.get(favorite[entry.getKey()]) - 2);
+        for (TwoPointCircleList t: twoList
+             ) {
+            twoMax += t.maxDistance();
         }
-        twoMax /= 2;
         return Math.max(twoMax, max);
+    }
+
+    private static List<TwoPointCircleList> getList(int[] favorite, Set<Integer> checked) {
+        List<TwoPointCircleList> lists = new ArrayList<>();
+        for (int i = 0; i < favorite.length; i++) {
+            if (checked.contains(i)) {continue;}
+            if (i == favorite[favorite[i]]) {
+                lists.add(new TwoPointCircleList(i, favorite[i]));
+                checked.add(i);
+                checked.add(favorite[i]);
+            }
+        }
+        return lists;
     }
 }
