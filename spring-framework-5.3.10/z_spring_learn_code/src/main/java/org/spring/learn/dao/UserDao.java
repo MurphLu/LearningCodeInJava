d21a01fb228ff73e40465b25e9d911df1e96a18c
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2023-12-01 10:42:05 +0800

    add leetode daily

--- spring-framework-5.3.10/z_spring_learn_code/src/main/java/org/spring/learn/dao/UserDao.java
+++ spring-framework-5.3.10/z_spring_learn_code/src/main/java/org/spring/learn/dao/UserDao.java
@@ -6,15 +6,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-
+/**
+ * spring 调用构造方法原则（有参的话，参数需要是 spring 维护的 bean，查找spring context 中 bean 的逻辑为先类型（同一类型多个的话）再名字）：
+ * 1. 如果类没有实现构造方法，那么使用默认构造方法（无参）
+ * 2. 如果类中有一个有参构造方法，那么使用已经实现的构造方法
+ * 3. 类实现多个构造方法
+ * 	  a. 如果构造方法都没有加 @Autowired，且有无参构造方法，那么使用无参构造方法
+ * 	  b. 如果构造方法都没有加 @Autowired，且没有无参构造方法，抛异常（找不到默认构造方法）
+ * 	  c. 如果有一个构造方法加 @Autowired，则使用该方法
+ * 	  d. 如果有多个构造方法加 @Autowired，抛异常
+ */
 @Component
 public class UserDao implements InitializingBean {
     @Autowired
     private User user;
 
-    @PostConstruct
+	public UserDao() {
+		System.out.println("user dao constructor without params");
+	}
+
+	public UserDao(User user) {
+		System.out.println("user dao constructor with one params");
+		this.user = user;
+	}
+
+	public UserDao(User user, User user1) {
+		System.out.println("user dao constructor with two params");
+		this.user = user;
+	}
+
+	@PostConstruct
     private void initialUser() {
-        user.setAdmin(true);
+		System.out.println("PostConstruct");
+        user.setAdmin(false);
     }
 
     public String getName() {
@@ -23,6 +47,7 @@ public class UserDao implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        user.setAdmin(false);
+		System.out.println("afterPropertiesSet");
+        user.setAdmin(true);
     }
 }
--- spring-framework-5.3.10/z_spring_learn_code/src/main/java/org/spring/learn/service/UserService.java
+++ spring-framework-5.3.10/z_spring_learn_code/src/main/java/org/spring/learn/service/UserService.java
@@ -4,6 +4,11 @@ import org.spring.learn.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+// class -> 无参构造方法 ->
+// 实例化 -> 依赖注入（属性赋值）->
+// 构造方法结束 @postConstruct ->
+// 一些初始化工作（InitializingBean->afterPropertiesSet）->
+// AOP -> if(AOP) 代理对象 -> Bean
 @Service
 public class UserService {
 
