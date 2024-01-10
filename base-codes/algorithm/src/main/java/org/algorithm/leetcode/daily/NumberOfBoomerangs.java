36ca089bd733680727a92747075031ccde783c24
murph.lu <l7022995@126.com>
2024-01-10 14:44:37 +0800

    add leetcode daily

--- base-codes/algorithm/src/main/java/org/algorithm/leetcode/daily/NumberOfBoomerangs.java
+++ base-codes/algorithm/src/main/java/org/algorithm/leetcode/daily/NumberOfBoomerangs.java
@@ -6,6 +6,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * 给定平面上 n 对 互不相同 的点 points ，其中 points[i] = [xi, yi] 。回旋镖 是由点 (i, j, k) 表示的元组 ，其中 i 和 j 之间的距离和 i 和 k 之间的欧式距离相等（需要考虑元组的顺序）。
+ *
+ * 返回平面上所有回旋镖的数量。
+ *
+ * 数组中两个点到其中一个点距离相等的组合，考虑顺序
+ */
 public class NumberOfBoomerangs {
     public static void main(String[] args) {
         int[][] points = StringToArray.generateArray("[[0,0],[1,0],[-1,0],[0,1],[0,-1]]");
