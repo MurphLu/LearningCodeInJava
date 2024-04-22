6560ecd78bab20d0f8cca37a28f5d13489014843
murph.lu <l7022995@126.com>
2024-04-22 10:26:59 +0800

    add leetcode daily

--- base-codes/algorithm/src/main/java/org/algorithm/leetcode/CombainationSum4.java
+++ base-codes/algorithm/src/main/java/org/algorithm/leetcode/CombainationSum4.java
@@ -6,6 +6,11 @@ import java.util.Arrays;
  * 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
  *
  * 题目数据保证答案符合 32 位整数范围。
+ *
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 1000
+ * nums 中的所有元素 互不相同
+ * 1 <= target <= 1000
  */
 public class CombainationSum4 {
     public static void main(String[] args) {
