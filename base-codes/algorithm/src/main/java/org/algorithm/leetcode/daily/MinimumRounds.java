d958a8e24f3ccb7176f3268afa4ba8f06cdfac28
murph.lu <l7022995@126.com>
2024-05-14 09:48:28 +0800

    add leetcode daily

--- base-codes/algorithm/src/main/java/org/algorithm/leetcode/daily/MinimumRounds.java
+++ base-codes/algorithm/src/main/java/org/algorithm/leetcode/daily/MinimumRounds.java
@@ -8,6 +8,10 @@ import java.util.Map;
  * 在每一轮中，你可以完成 2 个或者 3 个 相同难度级别 的任务。
  *
  * 返回完成所有任务需要的 最少 轮数，如果无法完成所有任务，返回 -1 。
+ *
+ * 统计每种任务个数，
+ * 大于 1 的话需要：int count = cnt / 3 + (cnt % 3 == 0 ? 0 : 1)
+ * 等于 1 那么一定无法完成，直接返回 -1
  */
 public class MinimumRounds {
     public int minimumRounds(int[] tasks) {
@@ -18,21 +22,9 @@ public class MinimumRounds {
 
         int round = 0;
         for(int i : map.values()) {
-            if (i == 1) {
-                return -1;
-            } else if (i == 2) {
-                round += 1;
-            } else {
-                round += getRound(i);
-            }
-        }
-        return round;
-    }
-
-    private int getRound(int n) {
-        int round = n/3;
-        if (n % 3 != 0) {
-            round += 1;
+            int temp = (i == 1 ? -1 : (i/3 + (i%3 == 0 ? 0 : 1)));
+            if (temp == -1){return -1;}
+            round += temp;
         }
         return round;
     }
