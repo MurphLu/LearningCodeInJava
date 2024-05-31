b6bcc7e0edd71a7219182f65a7098f0120e880ef
murph.lu <l7022995@126.com>
2024-05-31 15:43:02 +0800

    add leetcode MaxProfit

--- base-codes/algorithm/src/main/java/org/algorithm/leetcode/MaxProfit.java
+++ base-codes/algorithm/src/main/java/org/algorithm/leetcode/MaxProfit.java
@@ -71,8 +71,23 @@ public class MaxProfit {
      * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）
      * @param prices
      * @return
+     *
+     * 1,4,2,5
+     * 1,2,3,4,5
+     * 1,3,  2,4,  0,4,9, 8, 20
+     *  2     2      9      12
      */
+
+
     public int maxProfit2(int[] prices) {
-        return 0;
+        // 第一次买入，第一次卖出，第二次买入，第二次卖出
+        int f1 = -prices[0], f2 = 0, f3 = -prices[0], f4 = 0;
+        for (int i = 1; i < prices.length; ++i) {
+            f1 = Math.max(f1, -prices[i]);
+            f2 = Math.max(f2, f1 + prices[i]);
+            f3 = Math.max(f3, f2 - prices[i]);
+            f4 = Math.max(f4, f3 + prices[i]);
+        }
+        return f4;
     }
 }
