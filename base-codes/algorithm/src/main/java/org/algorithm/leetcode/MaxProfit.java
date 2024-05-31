108715e2f446fd3b9bc0972881929d5ea729db1c
murph.lu <l7022995@126.com>
2024-05-31 10:02:01 +0800

    add leetcode daily

--- base-codes/algorithm/src/main/java/org/algorithm/leetcode/MaxProfit.java
+++ base-codes/algorithm/src/main/java/org/algorithm/leetcode/MaxProfit.java
@@ -1,22 +1,26 @@
 package org.algorithm.leetcode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
 
-/**
- * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
- *
- * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
- *
- * 返回 你能获得的 最大 利润 。
- */
+
 public class MaxProfit {
     public static void main(String[] args) {
-        int res = new MaxProfit().maxProfit(new int[]{
-                7,1,5,3,6,4
+        int res = new MaxProfit().maxProfit2(new int[]{
+                7,6,4,3,1
         });
         System.out.println(res);
     }
+
+    /**
+     * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+     *
+     * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+     *
+     * 返回 你能获得的 最大 利润 。
+     */
     public int maxProfit(int[] prices) {
         LinkedList<Integer> stack = new LinkedList<>();
         int startIdx = 0;
@@ -57,5 +61,18 @@ public class MaxProfit {
             }
         }
         return res;
+    }
+
+    /**
+     * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+     *
+     * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+     *
+     * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int[] prices) {
+        return 0;
     }
 }
