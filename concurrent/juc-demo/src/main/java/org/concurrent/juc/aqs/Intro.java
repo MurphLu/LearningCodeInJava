29b1d22d8c1c8b64e7b7290f0a1b4c10c0a9e5fa
murph.lu <l7022995@126.com>
2024-06-13 09:27:43 +0800

    CountDownLatch

--- concurrent/juc-demo/src/main/java/org/concurrent/juc/aqs/Intro.java
+++ concurrent/juc-demo/src/main/java/org/concurrent/juc/aqs/Intro.java
@@ -1,10 +1,68 @@
 package org.concurrent.juc.aqs;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Intro {
     public static void main(String[] args) {
 
+//        reentrantLock();
+//        semaphore();
+
+        countDownLatch();
+    }
+
+    private static void countDownLatch() {
+        CountDownLatch startSingle = new CountDownLatch(1);
+        CountDownLatch doneSingle = new CountDownLatch(5);
+        for (int i = 0; i < 5; i++) {
+            new Thread(()->{
+                try {
+                    startSingle.await();
+                    Thread.sleep(1000);
+                    System.out.println(Thread.currentThread().getName() + " 开始执行...");
+                    doneSingle.countDown();
+                }catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }).start();
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startSingle.countDown();
+        try {
+            doneSingle.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("workDon and do something else");
+
+//        countDownLatch.countDown();
+    }
+
+    private static void semaphore() {
+        Semaphore semaphore = new Semaphore(3);
+        for (int i = 0; i < 100; i++) {
+            new Thread(()->{
+                try{
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + " 开始买票");
+                    Thread.sleep(1000);
+                    System.out.println(Thread.currentThread().getName() + " 买票结束");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+            }).start();
+        }
+    }
+
+    private static void reentrantLock() {
         ReentrantLock lock = new ReentrantLock(true);
 
         // 不一定回完全按照顺序执行
